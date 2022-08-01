4ef58ffd9ea308a1b0da74d5c4717e135b0cda7a
andriipanchuk <59779099+andriipanchuk@users.noreply.github.com>
2022-08-01 13:31:21 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sh /home/ubuntu/you/bot.py --visits 2 --url https://www.youtube.com/watch?v=wNC4YUa04hc --verbose
+python3 /home/ubuntu/you/bot.py --visits 2 --url https://www.youtube.com/watch?v=wNC4YUa04hc --verbose
